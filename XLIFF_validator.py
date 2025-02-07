f7fff043456d7e41f5342147cbb24bb812bae383
agomez <a@a>
2025-02-07 09:38:13 +0100

    Entry file

---
+++ XLIFF_validator.py
@@ -0,0 +1,15 @@
+﻿import sys
+import gettext
+from PySide6.QtWidgets import QApplication
+from src.gui.main_window import MainWindow
+
+# Initialize gettext for internationalization
+gettext.bindtextdomain("messages", "locale")
+gettext.textdomain("messages")
+_ = gettext.gettext
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
