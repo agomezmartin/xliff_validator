02229ab2f034a7f6fe21b3034391fb2d68f6e1a5
agomez <a@a>
2025-02-13 13:43:58 +0100

    style(i18n): Delete file

--- src/utils/i18n.py
+++
@@ -1,7 +0,0 @@
-﻿import gettext
-
-# ✅ Set the language for the application
-LANGUAGE = "fr"  # Change between "en" for English, "es" for Spanish or "fr" for French.
-
-translation = gettext.translation("messages", localedir="locale", languages=[LANGUAGE], fallback=True)
-gettext_gettext = translation.gettext  # ✅ Export this variable
