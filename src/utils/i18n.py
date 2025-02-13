628adbc6068e09edeb0126a722dfccd47ceb78de
agomez <a@a>
2025-02-13 13:44:48 +0100

    style(i18nmodule): Add i18n module for scalability purposes.

---
+++ src/utils/i18n.py
@@ -0,0 +1,7 @@
+﻿import gettext
+
+# ✅ Set the language for the application
+LANGUAGE = "en"  # Change between "en" for English, "es" for Spanish or "fr" for French.
+
+translation = gettext.translation("messages", localedir="locale", languages=[LANGUAGE], fallback=True)
+gettext_gettext = translation.gettext  # ✅ Export this variable
