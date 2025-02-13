4dfc95fd7b402ec8269322cb7a29b8e2460cb222
agomez <a@a>
2025-02-13 19:14:47 +0100

    i18n compiler and dev paths

--- src/utils/i18n.py
+++ src/utils/i18n.py
@@ -1,7 +1,19 @@
 ﻿import gettext
+import sys
+import os
 
 # ✅ Set the language for the application
-LANGUAGE = "en"  # Change between "en" for English, "es" for Spanish or "fr" for French.
+LANGUAGE = "fr"  # Change to "en" for English, "es" for Spanish or "fr" for French.
 
-translation = gettext.translation("messages", localedir="locale", languages=[LANGUAGE], fallback=True)
+# ✅ Determine the correct locale directory (FROM executable OR in development)
+if getattr(sys, 'frozen', False):  # Running as a PyInstaller bundle
+    base_path = sys._MEIPASS  # PyInstaller temporary extraction directory
+    localedir = os.path.join(base_path, "locale")  # Full path for "--onefile" compilation executable file
+#     localedir = os.path.join("_internal", "locale")  # Adjusted path for standard executable file (all files visible)
+else:
+    localedir = os.path.join(os.path.dirname(__file__), "locale")  # Full path for development mode
+    localedir = os.path.join("locale")  # Adjusted path for development mode
+
+# ✅ Initialize gettext
+translation = gettext.translation("messages", localedir=localedir, languages=[LANGUAGE], fallback=True)
 gettext_gettext = translation.gettext  # ✅ Export this variable
