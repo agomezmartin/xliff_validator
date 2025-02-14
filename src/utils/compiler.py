368fdefef09a1c4c6ffe3db2fb2ae4f1aa5a65ce
agomez <a@a>
2025-02-14 12:57:26 +0100

    chore(compilation): Isolate compilation

---
+++ src/utils/compiler.py
@@ -0,0 +1,11 @@
+﻿import sys
+import os
+
+# ✅ Determine the correct locale directory (FROM executable OR in development)
+if getattr(sys, 'frozen', False):  # Running as a PyInstaller bundle
+    base_path = sys._MEIPASS  # PyInstaller temporary extraction directory
+    localedir = os.path.join(base_path, "locale")  # Full path for "--onefile" compilation executable file
+#     localedir = os.path.join("_internal", "locale")  # Adjusted path for standard executable file (all files visible)
+else:
+    # localedir = os.path.join(os.path.dirname(__file__), "locale")  # Full path for development mode
+    localedir = os.path.join("locale")  # Adjusted path for development mode
