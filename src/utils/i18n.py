91dc001a928146c8174430810bc88aabcaa38d0f
agomez <a@a>
2025-02-26 14:04:48 +0100

    Set language to English

--- src/utils/i18n.py
+++ src/utils/i18n.py
@@ -2,7 +2,7 @@
 from src.utils.compiler import localedir # ✅ Import locale or _internal/locale variable
 
 # ✅ Set the language for the application
-LANGUAGE = "fr"  # Change to "en" for English, "es" for Spanish or "fr" for French.
+LANGUAGE = "en"  # Change to "en" for English, "es" for Spanish or "fr" for French.
 
 # ✅ Initialize gettext
 translation = gettext.translation("messages", localedir=localedir, languages=[LANGUAGE], fallback=True)
