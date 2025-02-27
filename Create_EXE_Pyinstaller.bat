7382505da3d5d1115ce9d579f7653f95f0e164c4
agomez <a@a>
2025-02-27 13:29:11 +0100

    Add pause to compiler bat file

--- Create_EXE_Pyinstaller.bat
+++ Create_EXE_Pyinstaller.bat
@@ -1,12 +1,14 @@
 @echo off
 REM This batch file compiles XLIFF_validator.py into a standalone .exe
 REM using PyInstaller with the specified options and icon.
-color 0B
+color 0A
 
 echo ===========================================================================
 echo This batch file compiles XLIFF_Validator into a standalone .exe
 echo using PyInstaller with the specified options and icon.
 echo ===========================================================================
+
+pause
 
 color 0B
 REM pyinstaller --log-level=DEBUG --noconsole --add-data "c:\xliff_validator\src\resources\*;src\img" --icon=c:\xliff_validator\src\resources\executable.png XLIFF_validator.py
